f24e6d3fd17696e20f83706f6535c95449aea7e7
Atul Vishwakarma <Atul.Vishwakarma@ezest.local>
2023-01-17 13:02:45 +0530

    social media url update

--- src/components/data/SocialMediaData.jsx
+++ src/components/data/SocialMediaData.jsx
@@ -3,13 +3,13 @@ export const SocialMediaData = [
   {
     id: 101,
     name: "Facebook",
-    profileurl: "https://www.facebook.com/",
+    profileurl: "https://www.facebook.com/aman.rai.16121?mibextid=ZbWKwL",
     icon: MdFacebook,
   },
   {
     id: 102,
     name: "Instagram",
-    profileurl: "https://www.instagram.com/",
+    profileurl: "https://www.instagram.com/invites/contact/?i=5l1r5tktdz8a&utm_content=3rqdfu8",
     icon: FiInstagram,
   },
   {
