4b16e75b8857fb991f9f321f6193fa1d71a95e94
Atul Vishwakarma <Atul.Vishwakarma@ezest.local>
2023-01-31 20:54:57 +0530

    Gallery jsx updated

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,22 +1,14 @@
 import { amanImg } from "../assests/aman/index";
+// import ModalPopUp from "../components/modal/ModalPopUp";
 import GalleryComp from "../components/utils/GalleryComp";
-import GallerySlider from "../components/utils/GallerySlider";
-import GallerySliderCard from "../components/utils/GallerySliderCard";
-import GallerySliderCube from "../components/utils/GallerySliderCube";
 
 const Gallery = () => {
   return (
     <div className="gallery-page">
       <div className="gallery-wrapper container max-w-full px-4">
         <div className="gallery-wrapper max-w-4xl mx-auto">
+          {/* <ModalPopUp /> */}
           <GalleryComp galleryData={amanImg} />
-          <br /><br />
-          <GallerySlider galleryData={amanImg} />
-          <br /><br />
-          <GallerySliderCard galleryData={amanImg} />
-          <br /><br />
-          <GallerySliderCube galleryData={amanImg} />
-          <br />
         </div>
       </div>
     </div>
